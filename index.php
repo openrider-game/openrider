0aa9228af3954cb40700883214c581bac00ff57b
tomiy <tom.chappaz1@gmail.com>
2023-10-23 12:09:21 +0200

    Fix vitejs bundling

--- index.php
+++ index.php
@@ -20,6 +20,12 @@
 
         GAME.newGame({<?= isset($_GET['track']) ? sprintf(' id: %s ', $_GET['track']) : '' ?>});
     </script>
+    <!-- If you wish to bundle the game with vitejs for production purposes, uncomment this part and comment the above script -->
+    <!-- Be sure to run `npm run build` beforehand to get the dist output file -->
+    <!-- <script src="./dist/openrider.umd.cjs"></script>
+    <script type="module">
+        OpenRider.GAME.newGame({<?= isset($_GET['track']) ? sprintf(' id: %s ', $_GET['track']) : '' ?>});
+    </script> -->
 </body>
 
 </html>
